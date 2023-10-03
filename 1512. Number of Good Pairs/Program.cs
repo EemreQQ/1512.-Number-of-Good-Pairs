387a962e7294a97bd08907c347b35c20dc2918e1
Emre <emre_erol17@hotmail.com>
2023-10-03 09:06:28 +0300

    Proje dosyası ekle.

---
+++ 1512. Number of Good Pairs/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+public class Solution
+{
+    public int NumIdenticalPairs(int[] nums)
+    {
+        Dictionary<int, int> d = new Dictionary<int, int>();
+        var total = 0;
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (d.ContainsKey(nums[i]))
+            {
+                d[nums[i]]++;
+                total += d[nums[i]];
+            }
+            else d.Add(nums[i], 0);
+        }
+        return total;
+    }
+}
